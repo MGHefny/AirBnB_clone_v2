1d79935e52188e8441327398b6b4a9c6808b5224
MGHefny <mgenedhefny@gmail.com>
2024-05-03 21:50:32 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,11 +4,11 @@
 from fabric.api import put, run, env
 from os.path import exists
 
-env.hosts = ['100.25.148.74', '100.25.38.55']
+env.hosts = ["100.25.148.74", "100.25.38.55"]
 
 
 def do_deploy(archive_path):
-    """ dis archive to web servers """
+    """dis archive to web servers"""
     if not exists(archive_path):
         print("Archive file does not exist:", archive_path)
         return False
@@ -17,15 +17,15 @@ def do_deploy(archive_path):
         nfile = archive_path.split("/")[-1]
         nexit = nfile.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, nexit))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(nfile, path, nexit))
-        run('rm /tmp/{}'.format(nfile))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, nexit))
-        run('rm -rf {}{}/web_static'.format(path, nexit))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, nexit))
-        
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}/".format(path, nexit))
+        run("tar -xzf /tmp/{} -C {}{}/".format(nfile, path, nexit))
+        run("rm /tmp/{}".format(nfile))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, nexit))
+        run("rm -rf {}{}/web_static".format(path, nexit))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, nexit))
+
         return True
     except Exception as x:
         print("Error deploying:", x)
