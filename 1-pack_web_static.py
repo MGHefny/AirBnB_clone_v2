f3a28627704d1dfc733b5b57f5c5609ed288d9e5
MGHefny <mgenedhefny@gmail.com>
2024-05-03 21:35:31 +0300

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,16 +3,16 @@
 
 from datetime import datetime
 from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
     """ gene tgz """
- t = datetime.now()
+    t = datetime.now()
     arch = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     crt = local('tar -cvzf versions/{} web_static'.format(arch))
-    if crt is not None:
+    if crt.succeeded:
         return arch
     else:
         return None
+
