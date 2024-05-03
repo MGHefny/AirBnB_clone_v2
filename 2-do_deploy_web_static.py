238e99c606fb160bb2009ae0aa42c11872f1399e
MGHefny <mgenedhefny@gmail.com>
2024-05-03 21:49:06 +0300

    2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Fabric 1-pack_web_static.py web servers """
+
+from fabric.api import put, run, env
+from os.path import exists
+
+env.hosts = ["100.25.148.74", "100.25.38.55"]
+
+
+def do_deploy(archive_path):
+    """dis archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        nfile = archive_path.split("/")[-1]
+        nexit = nfile.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}/".format(path, nexit))
+        run("tar -xzf /tmp/{} -C {}{}/".format(nfile, path, nexit))
+        run("rm /tmp/{}".format(nfile))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, nexit))
+        run("rm -rf {}{}/web_static".format(path, nexit))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, nexit))
+        return True
+    except:
+        return False
