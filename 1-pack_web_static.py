8e5674bcf8c719a823240b8ac82d430c372acc6e
MGHefny <mgenedhefny@gmail.com>
2024-05-03 21:36:26 +0300

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,12 @@ from fabric.api import local
 
 
 def do_pack():
-    """ gene tgz """
+    """gene tgz"""
     t = datetime.now()
-    arch = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    crt = local('tar -cvzf versions/{} web_static'.format(arch))
+    arch = "web_static_" + t.strftime("%Y%m%d%H%M%S") + "." + "tgz"
+    local("mkdir -p versions")
+    crt = local("tar -cvzf versions/{} web_static".format(arch))
     if crt.succeeded:
         return arch
     else:
         return None
-
