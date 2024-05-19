bd4e2301f1f99d5b5a895b3da4637cc6387020d5
MGHefny <mgenedhefny@gmail.com>
2024-05-19 23:46:00 +0300

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def inumb(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def numbtemp(n):
-    return render_templates("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
