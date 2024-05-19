eb613f49ef7ae7c7f645bf5dc2e5ef2dd5478f17
MGHefny <mgenedhefny@gmail.com>
2024-05-19 23:40:35 +0300

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,38 +2,39 @@
 """Flask app"""
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def ind():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hb():
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def cfun(text):
-    return 'C' + text.replace('_', ' ')
+    return "C" + text.replace("_", " ")
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def pythcool(text='is cool'):
-    return 'Python ' + text.replace('_', ' ')
+@app.route("/python/<text>", strict_slashes=False)
+def pythcool(text="is cool"):
+    return "Python " + text.replace("_", " ")
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def inumb(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def numbtemp(n):
-    return render_template('5-number.html', n=n)
+    return render_template("5-number.html", n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
