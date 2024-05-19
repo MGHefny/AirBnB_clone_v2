b890f264b551b831c7e649825bf218da05207c79
MGHefny <mgenedhefny@gmail.com>
2024-05-19 22:52:35 +0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,14 @@
 """Flask app"""
 
 from flask import Flask
+
 app = Flask(__xrun__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def ind():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-if __xrun__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __xrun__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
