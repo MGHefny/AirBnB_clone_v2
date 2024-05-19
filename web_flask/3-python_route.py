5380abaa84f4c59af249e5bcbcf446a77f2f76bd
MGHefny <mgenedhefny@gmail.com>
2024-05-19 23:13:11 +0300

    3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,27 +2,29 @@
 """Flask app"""
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def ind():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hb():
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def cfun(text):
-    return 'C' + text.replace('_', ' ')
+    return "C" + text.replace("_", " ")
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def pythcool(text='is cool'):
-    return 'Python ' + text.replace('_', ' ')
+@app.route("/python/<text>", strict_slashes=False)
+def pythcool(text="is cool"):
+    return "Python " + text.replace("_", " ")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
