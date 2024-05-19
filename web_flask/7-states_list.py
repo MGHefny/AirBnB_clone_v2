e0554e4d7c2007a6afbd57408532272285a187c1
MGHefny <mgenedhefny@gmail.com>
2024-05-20 01:59:59 +0300

    0

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Flask app"""
+
+from flask import Flask, render_template
+from models import storage
+from models import *
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    stat = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', stat=stat)
+
+
+@app.teardown_appcontext
+def db_dow(exception):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
