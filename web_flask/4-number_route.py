11b8ed885200a7355cb7b7b33b865d2cb739c635
MGHefny <mgenedhefny@gmail.com>
2024-05-19 23:20:46 +0300

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def ind():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    return 'C' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythcool(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def inumb(n):
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
