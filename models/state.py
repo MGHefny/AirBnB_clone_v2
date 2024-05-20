a8ea0c43fec89f35fdceeaa2ea04eb94459d30fc
MGHefny <mgenedhefny@gmail.com>
2024-05-20 03:27:18 +0300

    stat

--- models/state.py
+++ models/state.py
@@ -1,33 +1,8 @@
 #!/usr/bin/python3
-"""State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """Representation"""
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            city_l = []
-            all_c = models.storage.all(City)
-            for city in all_c.values():
-                if city.state_id == self.id:
-                    city_l.append(city)
-            return city_l
+class State(BaseModel):
+    """ State class """
+    name = ""
